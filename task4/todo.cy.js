01c7e0f14c6e916014d8677b51253cd916a3d67b
Голубев Никита <nikitagolybev250505@gmail.com>
2024-11-09 12:30:49 +0400

    first commi1t

--- task4/todo.cy.js
+++ task4/todo.cy.js
@@ -1,70 +1,46 @@
 /// <reference types="cypress" />
 
-describe('Advanced Amazon functionality tests', () => {
+describe('The Internet functionality tests', () => {
   beforeEach(() => {
-    cy.visit('https://www.amazon.com') // Посещение сайта Amazon
-  })
+    cy.visit('https://the-internet.herokuapp.com/');
+  });
 
-  it('.login() - log into Amazon account', () => {
-    cy.get('#nav-link-accountList-nav-line-1').click() // Клик по ссылке входа
-    cy.get('input[name="email"]').type('your-email@example.com')
-    cy.get('input[name="password"]').type('your-password')
-    cy.get('#signInSubmit').click() // Подтверждение входа
-    cy.get('#nav-link-accountList-nav-line-1').should('contain', 'Account & Lists') // Проверка успешного входа
-  })
+  it('.checkHeading() - проверить заголовок на главной странице', () => {
+    cy.get('h1').should('contain', 'Welcome to the-internet');
+    cy.get('h2').should('exist'); // Проверка наличия заголовка h2
+    cy.getCookies().then((cookies) => {
+      expect(cookies).to.have.length.above(0); // Проверка наличия cookies
+      cookies.forEach(cookie => {
+        expect(cookie).to.have.property('name').and.to.not.be.empty; // Проверка имени cookie
+        expect(cookie).to.have.property('value').and.to.not.be.empty; // Проверка значения cookie
+      });
+    });
+  });
 
-  it('.viewProfile() - view and edit user profile', () => {
-    cy.get('#nav-link-accountList-nav-line-1').click() // Клик по ссылке профиля
-    cy.get('a').contains('Your Account').click()
-    cy.url().should('include', 'gp/css/homepage.html') // Проверка URL страницы профиля
-    cy.get('a').contains('Login & security').click()
-    cy.url().should('include', 'ap/cnep?_encoding') // Проверка URL страницы редактирования
-    cy.get('input[name="email"]').should('have.value', 'your-email@example.com')
-    // Для безопасности внесение изменений опущено
-  })
+  it('.checkLinkHref() - проверить наличие ссылки на страницу с аутентификацией', () => {
+    cy.get('a').contains('Basic Auth')
+      .should('have.attr', 'href', '/basic_auth')
+      .and('have.text', 'Basic Auth'); // Проверка наличия ссылки с текстом
+    cy.get('a').contains('Basic Auth').click(); // Переход по ссылке
+    cy.url().should('include', '/basic_auth'); // Проверка URL
+  });
 
-  it('.addMultipleItemsToCart() - add multiple items to the cart and verify', () => {
-    cy.get('#twotabsearchtextbox').type('laptop{enter}')
-    cy.get('.s-main-slot .s-result-item').first().find('span[data-component-type="s-product-image"] a').click()
-    cy.get('#add-to-cart-button').click() // Добавление первого товара в корзину
-    cy.wait(1000)
-    cy.get('#twotabsearchtextbox').type('mouse{enter}')
-    cy.get('.s-main-slot .s-result-item').first().find('span[data-component-type="s-product-image"] a').click()
-    cy.get('#add-to-cart-button').click() // Добавление второго товара в корзину
-    cy.get('.nav-cart-count').should('have.text', '2') // Проверка количества товаров в корзине
-  })
+  it('.basicAuth() - проверить базовую аутентификацию', () => {
+    cy.visit('https://admin:admin@the-internet.herokuapp.com/basic_auth');
+    cy.get('p').should('contain', 'Congratulations! You must have the proper credentials.');
+  });
 
-  it('.navigateAndSort() - navigate to a category and sort products', () => {
-    cy.get('#nav-hamburger-menu').click() // Открытие меню
-    cy.get('.hmenu-visible a').contains('Electronics').click() // Переход в категорию
-    cy.get('a').contains('Headphones').click() // Переход в подкатегорию
-    cy.url().should('include', 'electronics-accessories') // Проверка URL
-    cy.get('span.a-dropdown-label').contains('Sort by').click()
-    cy.get('a').contains('Price: High to Low').click() // Сортировка по цене
-    cy.wait(2000) // Ожидание применения сортировки
-    cy.get('.s-main-slot .s-result-item').first().find('.a-price-whole').invoke('text').then(parseFloat).should('be.above', 100) // Проверка цены
-  })
+  it('.addRemoveElements() - добавить и удалить элементы', () => {
+    cy.get('a').contains('Add/Remove Elements').click();
+    cy.get('button').contains('Add Element').click();
+    cy.get('.added-manually').should('have.length', 1);
+    cy.get('.added-manually').click();
+    cy.get('.added-manually').should('have.length', 0);
+  });
 
-  it('.placeOrder() - complete a purchase', () => {
-    cy.get('#twotabsearchtextbox').type('Echo Dot{enter}')
-    cy.get('.s-main-slot .s-result-item').first().find('span[data-component-type="s-product-image"] a').click()
-    cy.get('#add-to-cart-button').click() // Добавление товара в корзину
-    cy.get('.nav-cart-icon').click() // Переход в корзину
-    cy.get('input[name="proceedToRetailCheckout"]').click() // Переход к оформлению заказа
-
-    // Авторизация пользователя (если не выполнена)
-    cy.get('input[name="email"]').type('your-email@example.com')
-    cy.get('input[name="password"]').type('your-password')
-    cy.get('input#signInSubmit').click()
-
-    // Проверка и подтверждение адреса доставки
-    cy.get('input.a-button-input').contains('Use this address').click()
-
-    // Выбор способа оплаты и оформление заказа
-    cy.get('input.a-button-input').contains('Use this payment method').click()
-    cy.get('input.a-button-input').contains('Place your order').click()
-
-    // Проверка успешного оформления заказа
-    cy.get('h1').should('contain', 'Thank you, your order has been placed.')
-  })
-})
+  it('.checkCheckbox() - отметить и снять галочку', () => {
+    cy.get('a').contains('Checkboxes').click();
+    cy.get('input[type="checkbox"]').first().check().should('be.checked');
+    cy.get('input[type="checkbox"]').first().uncheck().should('not.be.checked');
+  });
+});
